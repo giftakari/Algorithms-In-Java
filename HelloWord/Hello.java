0ae24221a09044feed68ad5fca22cbcee79a018c
Godsgift ThankGod Akari <giftakari@yahoo.com>
2019-07-18 12:34:24 +1000

    Starter files

---
+++ HelloWord/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,  ");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        System.out.print(" Welcome to Akaris Travels");
+
+    }
+}
---
+++ Ruller/Ruller.java
@@ -0,0 +1,15 @@
+public class Ruller {
+
+    public static void main(String[] args) {
+        String A = "1";
+        String B = A + "2" + A;
+        String C = B + "3" + B;
+        String D = C + "4" + C;
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.print(D);
+
+    }
+
+}
